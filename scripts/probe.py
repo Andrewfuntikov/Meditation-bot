0a4b5f4252af5f8825ebe49b87d88d5bacf4cd7b
Андрей <andhunter77@gmail.com>
2024-06-23 20:57:09 +0500

    Не работает 2 конец дня

--- scripts/probe.py
+++ scripts/probe.py
@@ -1,21 +1,17 @@
 """Здесь будет рабочий проект без интеграции в тг бота"""
 from pydub import AudioSegment
 
-sounds = []  # Создаем пустой список для хранения аудиофайлов
+sounds = []
 eee = input('Да/нет ')
 while eee != 'нет':
-    # Запросить название аудиофайла
     file_name = input('название файла: ')
 
-    # Загружаем аудиофайл и добавляем его в список
     sound = AudioSegment.from_mp3(file_name)
     sounds.append(sound)
 
-    eee = input('Да.нет ')
+    eee = input('Да/нет ')
 
-# Склеиваем все аудиофайлы из списка
 combined_sound = sum(sounds)
-# Экспортируем склеенный аудиофайл
 combined_sound.export("lox.mp3", format="mp3")
 
 print('stop')
