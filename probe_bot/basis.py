1aee4909602eefddb4eda2af95ba4eccc7ab7702
Андрей <andhunter77@gmail.com>
2024-06-26 22:10:25 +0500

    Добавил ункальные имена для скачивания голосовых, однако с  голосовыми осталась

--- probe_bot/basis.py
+++ probe_bot/basis.py
@@ -3,10 +3,18 @@ from aiogram.types import Message, FSInputFile
 from pydub import AudioSegment
 import os
 import time
+import random
 
 sounds_pydub_format = []
 
 sounds_name = []
+
+random_symbol_list = [
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';',
+    ':', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ','
+]
 
 
 async def download(message: Message, bot: Bot):
@@ -23,14 +31,16 @@ async def download(message: Message, bot: Bot):
         sounds_pydub_format.append(sound)
         await message.answer('Спасибо за аудиосообщение! Теперь отправьте ещё, и я их склею.')
     elif message.content_type == 'voice':
+        random_symbol = random.sample(random_symbol_list, 10)
+        random_symbol_string = ''.join(random_symbol)
         file_id = message.voice.file_id
         file = await bot.get_file(file_id)
         user_id = message.from_user.id
         file_path = file.file_path
-        file_name_ex = f'{user_id}_VOICE_.mp3'
+        file_name_ex = f'{user_id}_VOICE_{random_symbol_string}.mp3'
         await bot.download_file(file_path, file_name_ex)
-        sound = AudioSegment.from_mp3(file_name_ex)
         sounds_name.append(file_name_ex)
+        sound = AudioSegment.from_mp3(file_name_ex)
         sounds_pydub_format.append(sound)
         await message.answer('Спасибо за аудиосообщение! Теперь отправьте ещё, и я их склею.')
     elif message.text.lower() == 'хватит':
@@ -43,17 +53,20 @@ async def download(message: Message, bot: Bot):
         try:
             await bot.send_audio(audio=FSInputFile(file_name_ex), chat_id=user_id)
             await message.answer('Спасибо что воспользовались моим ботом!')
-            await bot.send_message(5867884661, text=f'Ботом воспользовался username = {user_name}, user_id = {user_id}')
+            await bot.send_message(5867884661,
+                                   text=f'Ботом воспользовался username = {user_name}, user_id = {user_id}')
         except Exception as exc:
-            print(f'ERROR ERROR ERROR ERROR {exc}')
             await message.answer('Ошибка! Похоже у меня технические шоколадки(')
+            await bot.send_message(5867884661,
+                                   text=f'Ошибка у пользователя  username = {user_name}, user_id = {user_id}, ошибка = {exc}')
         time.sleep(20)
         try:
             os.remove(file_name_ex)
             for file in sounds_name:
                 os.remove(file)
         except Exception as exc:
-            print(f'ERROR {exc}')
+            await bot.send_message(5867884661,
+                                   text=f'Не удалилсь файлы username = {user_name}, user_id = {user_id}')
         finally:
             sounds_pydub_format.clear()
             sounds_name.clear()
